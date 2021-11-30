44a870ae7e1e2b5182ee188c6d3adf8b6d9af265
ChayanGit <chayan.choudhurypersonal@gmail.com>
2021-11-30 21:02:39 +0530

    adding cors config

--- react-client/src/http-common.js
+++ react-client/src/http-common.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://3.140.240.106:8080/api",
   headers: {
+    "Cache-Control":"no-cache",
+    "Accept-language": "en",
     "Content-type": "application/json",
-    "Access-Control-Allow-Origin": "http://3.140.240.106:8080"
   }
 });
