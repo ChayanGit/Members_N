083dfda0322fe6152523a86fdcb55011eb177a04
root <root@ip-172-31-29-63.us-east-2.compute.internal>
2021-12-01 17:13:32 +0000

    build folder

---
+++ react-client/build/precache-manifest.24ae2f3e6c0de1811d49f30ff5c287fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4241b1abb2ad47521048cf4c5dc8c6e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf0193a8fc2a0016205a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "198a5a6524e4e7f122a5",
+    "url": "/static/css/main.b5fec8d3.chunk.css"
+  },
+  {
+    "revision": "cf0193a8fc2a0016205a",
+    "url": "/static/js/2.8091b331.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.8091b331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "198a5a6524e4e7f122a5",
+    "url": "/static/js/main.b51baa12.chunk.js"
+  },
+  {
+    "revision": "07858fea14fd66493418",
+    "url": "/static/js/runtime-main.efee8004.js"
+  }
+]);
